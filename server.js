7dc9a1a4fef87a757fe3aec5773e941e764fffab
Timo <me@timoheijne.nl>
2018-03-07 09:43:55 +0100

    Fixed port

--- server.js
+++ server.js
@@ -23,4 +23,4 @@ io.on('connection', function(socket){
     });
 });
 
-http.listen(app.env.PORT || 3000, () => console.log('Example app listening on port 3000!'))
+http.listen(process.env.PORT || 3000, () => console.log('Example app listening on port 3000!'))
